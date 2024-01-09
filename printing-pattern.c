89e2cc969eee38bc0dd4e8c03f4cb006f6cacab2
Cold-Coffee-Coder <92594389+Cold-Coffee-Coder@users.noreply.github.com>
2024-01-09 22:11:11 +0530

    Add files via upload

---
+++ printing-pattern.c
@@ -0,0 +1,23 @@
+/*PRINT THE PATTERN AS FOLLOWS
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+
+#include<stdio.h>
+
+int main(){
+    int n,i,j,num=1;
+    printf("Number of lines : ");
+    scanf("%d",&n);
+    for (i=1;i<n+1;i++){
+        for (j=1;j<i+1;j++){
+            printf("%d ",num);
+            num++;
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
